1acb4b27d8bc4cb52798807f3d4b06361e09be53
krating20 <56456483+krating20@users.noreply.github.com>
2024-02-06 21:35:06 +0700

    single message chatbot

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import openai from './config/open-ai.js'
 import readlineSync from 'readline-sync';
 import colors from 'colors';
 async function main() {
+    /*
     const chatCompletion = await openai.chat.completions.create({
         model: 'gpt-3.5-turbo',
         messages:[
@@ -10,6 +11,28 @@ async function main() {
     });
 
     console.log(chatCompletion.choices[0].message.content);
+    */
+   console.log(colors.bold.green('welcome to Kratings Chatbot'));
+   while (true){
+    const userInput = readlineSync.question(colors.yellow('You: '))
+
+    try {
+        //call the API with user input
+        const completion =await openai.chat.completions.create({
+            model: "gpt-3.5-turbo",
+            messages: [{"role": "user", "content": userInput}],
+        })
+        //get  completion text
+        const completionText = completion.choices[0].message.content
+        if(userInput.toLowerCase() ==='exit'){
+            console.log(colors.green('Bot: ')+ completionText);
+            return;
+        }
+        console.log(colors.green('Bot: ')+ completionText);
+    } catch (error) {
+        console.error(colors.red(error));
+    }
+   }
 }
 
 main();
